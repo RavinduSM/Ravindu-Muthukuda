de1f1fab0cdbd215a221ac72ebb03addc27ccec8
RavinduSM <ravindumuthukudaarachchi@gmail.com>
2025-03-05 20:39:20 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // Use relative paths for assets
   build: {
-    outDir: 'dist',  // Make sure the built files go to "dist"
+    outDir: "dist",
   },
-})
+});
